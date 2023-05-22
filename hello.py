3efd39e7c45582bce2f304e8067d74486d188d13
David Cardoso <david.lima.cd@gmail.com>
2023-05-22 13:08:28 -0300

    Insert GetObject S3

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
+import boto3
+
 def hello_function(event, context):
     print("Hello Terraform")
 
-def secondValues():
-    print("Test 2")
+    try:
+        print(getS3Object())
+    except Exception as err:
+        print(err)
+
+def getS3Object():
+    s3 = boto3.resource('s3')
+    bucket = 'my-first-bucket-7507'
+    obj = s3.Bucket(bucket).Object('base.json')
+    getData = obj.get()
+    s3data = getData['Body'].read().decode()
+    return s3data
+
